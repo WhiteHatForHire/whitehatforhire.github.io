838a50032ba991672bdf642b76de009ba5facc11
Maxwell Stephens <stephensm2011@gmail.com>
2017-10-23 01:49:32 -0400

    first commit

---
+++ p5 Fractal1/sketch.js
@@ -0,0 +1,69 @@
+var angle = 0, color = 0;
+var mult1 = 0.67;
+var mySpan, mySpan2;
+var slider, colorslider, translider;
+var wildcolor, wildcolor2, shift;
+wildcolor = randomcolor();
+wildcolor2 = randomcolor();
+var button;
+function setup() {
+  randomcolor();
+  createCanvas(600,600);
+  slider = createSlider(0, 3.14159265359, 3.14159265359 / 4, 0.05);
+  slider.parent('myContainer');
+  mySpan = createSpan('Position');
+  mySpan.parent('myContainer');
+  colorslider = createSlider(0, 255, 0, 1);
+  colorslider.parent('myContainer1');
+  translider = createSlider(-25, 25, 0, 1);
+  translider.parent('myContainer2');
+  mySpan2 = createSpan('Color');
+  mySpan2.parent('myContainer1');
+  mySpan2 = createSpan('Shift');
+  mySpan2.parent('myContainer2');
+  button = createButton('Randomize Color');
+  button.parent('myContainer3');
+  button.mousePressed(randomize);
+}
+function draw() {
+  background(0);
+  angle = slider.value();
+  color = colorslider.value();
+  shift = translider.value();
+  var len = 100;
+  stroke(wildcolor, wildcolor2, color, 255);
+  translate(300, height);
+  branch(175);
+}
+
+function branch(len) {
+  line(0, 0, 0, -len);
+  translate(shift, -len);
+  if (len > 4){
+    push();
+    rotate(angle);
+    branch(len * mult1);
+    pop();
+    push();
+    rotate(-angle);
+    branch(len * mult1);
+    pop();
+    }
+}
+
+
+
+function randomcolor () {
+  var i;
+  i = Math.random() * 255;
+  return i;
+  console.log(i);
+}
+
+function randomize (){
+  wildcolor = Math.random() * 255;
+}
+
+
+
+
