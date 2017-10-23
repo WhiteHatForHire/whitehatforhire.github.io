d477a25fd6dc3d7ed462c9aa3af0a2fd46b71ab6
Maxwell Stephens <stephensm2011@gmail.com>
2017-10-23 02:11:26 -0400

    added slider for size, tweaked values

--- p5 Fractal1/sketch.js
+++ p5 Fractal1/sketch.js
@@ -1,26 +1,30 @@
 var angle = 0, color = 0;
 var mult1 = 0.67;
-var mySpan, mySpan2;
-var slider, colorslider, translider;
-var wildcolor, wildcolor2, shift;
+var mySpan, mySpan2, myspan3;
+var slider, colorslider, translider, sizeslider;
+var wildcolor, wildcolor2, shift, size;
 wildcolor = randomcolor();
 wildcolor2 = randomcolor();
 var button;
 function setup() {
   randomcolor();
-  createCanvas(600,600);
+  createCanvas(800,600);
   slider = createSlider(0, 3.14159265359, 3.14159265359 / 4, 0.05);
   slider.parent('myContainer');
   mySpan = createSpan('Position');
   mySpan.parent('myContainer');
   colorslider = createSlider(0, 255, 0, 1);
   colorslider.parent('myContainer1');
-  translider = createSlider(-25, 25, 0, 1);
+  translider = createSlider(-50, 50, 0, 1);
   translider.parent('myContainer2');
   mySpan2 = createSpan('Color');
   mySpan2.parent('myContainer1');
   mySpan2 = createSpan('Shift');
   mySpan2.parent('myContainer2');
+  sizeslider = createSlider(0, 400, 150, 1);
+  sizeslider.parent('myContainer4');
+  mySpan3 = createSpan('Size');
+  mySpan3.parent('myContainer4');
   button = createButton('Randomize Color');
   button.parent('myContainer3');
   button.mousePressed(randomize);
@@ -30,16 +34,17 @@ function draw() {
   angle = slider.value();
   color = colorslider.value();
   shift = translider.value();
+  size = sizeslider.value();
   var len = 100;
   stroke(wildcolor, wildcolor2, color, 255);
-  translate(300, height);
-  branch(175);
+  translate(400, height);
+  branch(size);
 }
 
 function branch(len) {
   line(0, 0, 0, -len);
   translate(shift, -len);
-  if (len > 4){
+  if (len > 4 ){
     push();
     rotate(angle);
     branch(len * mult1);
